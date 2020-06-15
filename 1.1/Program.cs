ae5f2924638ac0808311f3dae719b69d7bdeef4b
David Oliveira <luiz.david@cencosud.com.br>
2020-06-15 08:35:00 -0300

    Master

---
+++ 1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a palavra desejada: ");
+            string palavra = Console.ReadLine();
+            
+        }
+    }
+}
