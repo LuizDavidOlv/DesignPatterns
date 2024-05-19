1e3f4369a3167b4b7a00f853b2935e8272e1d609
David Oliveira <Luiz.Oliveira2@Dell.com>
2024-05-19 11:23:09 -0400

    SRP example addded

---
+++ 1.1/Solid/Single Resposability/Email Example/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._1.Solid.Single_Resposability
+{
+    public class User
+    {
+        public int ClienteId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Password { get; set; }
+
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+    }
+
+
+}
